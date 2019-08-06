3ac301c3b3566d33ed17ebd2a546d57483cf99c0
Phil Hargett <phil@haphazardhouse.net>
2019-08-05 21:52:12 -0700

    initial commit

---
+++ src/RxJulia.jl
@@ -0,0 +1,107 @@
+module RxJulia
+
+export greet, @rx, Reactor, slot
+
+greet() = print("hello, world!")
+
+"""
+A `Reactor` is an `Observable` that is also useful for building `Observer`s that
+are `Observable` as well.
+"""
+mutable struct Reactor 
+    op
+    observers::Array{Any,1}
+    state
+end
+
+Reactor() = Reactor(pass, [])
+
+function subscribe!(reactor::Reactor, observer)
+    push!(reactor.observers, observer)
+    observer
+end
+
+function onEvent(reactor::Reactor, value)
+    let observers = reactor.observers
+        op = reactor.op
+        op(observers, value)
+    end
+end
+
+function onComplete(reactor::Reactor)
+    for observer in reactor.observers
+        onComplete(observer)
+    end
+end
+
+function onError(reactor::Reactor, e)
+    for observer in reactor.observers
+        onError(observer, e)
+    end
+end
+
+function pass(observers, value)
+    for observer in observers 
+        onEvent(observer, value)
+    end
+end
+
+"""
+Ssubscribe the `observer` to the `observable`, and return the `observable`.
+"""
+function chain!(observable, observer)
+    subscribe!(observable, observer)
+    observable
+end
+
+"""
+Given a do-block where each statement is an observable, 
+subscribe each in sequence to the one proceeding, and the
+first one to `start`.
+
+Example:
+
+  ```
+  @rx() do
+    count
+    filter(:event)
+  end`
+  ```
+"""
+macro rx(blk)
+    # the blk is actually an expression of type :-> (closure),
+    # and its first args is the args list (should be () ), followed by
+    # the array of statements in the closure's block
+    steps = reverse(blk.args[2].args)
+    last = steps[1]
+    pipes = map(steps[2:end]) do p
+        if typeof(p) == Expr
+            :( it = chain!($p, it) )
+        else
+            p
+        end
+    end
+    return quote
+        let it = $last
+            $(pipes...)
+        end
+    end
+end
+
+"""
+Create a `Reactor` useful as the initial `Observable` in a `reactor`.
+"""
+function slot(initial)
+    Reactor(pass, [], initial)
+end
+
+function setSlotValue(s, value)
+    s.state = value
+    onEvent(s.reactor, getSlotValue(s))
+end
+
+function getSlotValue(s)
+    s.state
+end
+
+end # module
