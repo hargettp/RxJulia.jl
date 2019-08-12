c4b86c20df843cd3d45af2cca581d165c3647731
Phil Hargett <phil@haphazardhouse.net>
2019-08-12 07:31:50 -0700

    refining code organization, new unit test

--- src/RxJulia.jl
+++ src/RxJulia.jl
@@ -3,7 +3,7 @@ module RxJulia
 export 
     @rx, Reactor, slot, Event, ValueEvent, CompletedEvent,
     ErrorEvent, Observer, Observable, events,
-    onEvent, onValue, onComplete, onError
+    onEvent, onValue, onComplete, onError, subscribe!, notify!
 
 struct ValueEvent{T}
     value::T
@@ -30,6 +30,31 @@ An `Observer` is a receiver of `Event`s via `onEvent`
 abstract type Observer end
 
 """
+Deliver an `Event` to the `Observer`
+"""
+function onEvent(observer, event)
+    if isa(event, ValueEvent)
+        onValue(observer, event.value)
+    elseif isa(event, CompletedEvent)
+        onComplete(observer)
+    else 
+        onError(observer, event)
+    end
+end
+
+"""
+By default observers do nothing upon receiving a `CompletedEvent`
+"""
+function onComplete(observer::Observer)
+end
+
+"""
+By default observers do nothing upon receiving an `ErrorEvent`
+"""
+function onError(observer::Observer, event::ErrorEvent)
+end
+
+"""
 `Observers` is a collection of subscribed `Observer`s
 """
 Observers = Array{Observer,1}
@@ -40,10 +65,29 @@ end
 
 Observable() = Observable([])
 
-function notify!(observable::Observable, evt::Event)
-    for observer in observable.observers
-        onEvent(observer, evt)
+"""
+Notify `Observer``` by invoking the block on each
+"""
+function notify!(observers, event)
+    for observer in observers
+        onEvent(observer, event)
     end
+end
+
+"""
+Subscribe an `Observer` to the given `Observable`
+"""
+function subscribe!(observable::Observable, observer)
+    push!(observable.observers, observer)
+    observer
+end
+
+"""
+Subscribe the `observer` to the `observable`, and return the `observable`.
+"""
+function chain!(observable, observer)
+    subscribe!(observable, observer)
+    observable
 end
 
 """
@@ -75,33 +119,8 @@ function Base.getproperty(reactor::Reactor, field::Symbol)
     end
 end
 
-"""
-Deliver an `Event` to the `Observer`
-"""
-function onEvent(observer, event)
-    if isa(event, ValueEvent)
-        onValue(observer, event.value)
-    elseif isa(event, CompletedEvent)
-        onComplete(observer)
-    else 
-        onError(observer, event)
-    end
-end
-
 function onValue(reactor::Reactor, value)
     reactor.op(reactor.observers, value)
-end
-
-"""
-By default observers do nothing upon receiving a `CompletedEvent`
-"""
-function onComplete(observer::Observer)
-end
-
-"""
-By default observers do nothing upon receiving an `ErrorEvent`
-"""
-function onError(observer::Observer, event::ErrorEvent)
 end
 
 """
@@ -122,95 +141,8 @@ function onError(reactor::Reactor, e)
     end
 end
 
-"""
-Subscribe an `Observer` to the given `Observable`
-"""
-function subscribe!(observable::Observable, observer)
-    push!(observable.observers, observer)
-    observer
-end
-
 function subscribe!(reactor::Reactor, observer)
     subscribe!(reactor.observable, observer)
-    observer
-end
-
-"""
-Notify `Observer``` by invoking the block on each
-"""
-function notify(blk, observers, event)
-    for observer in observers
-        blk(observer, event)
-    end
-end
-
-"""
-Subscribe the `observer` to the `observable`, and return the `observable`.
-"""
-function chain!(observable, observer)
-    subscribe!(observable, observer)
-    observable
-end
-
-"""
-Given a do-block where each statement is an `Observable`, 
-`subscribe!` each in sequence to the one proceeding. Return an
-object that one can use to iterate over the events from
-the last `Observable` in the block.
-
-Example:
-
-  ```
-  results = @rx() do
-    count
-    filter(:even)
-  end
-  for evt in results
-    # do something with evt
-  end
-  ```
-"""
-macro rx(blk)
-    # the blk is actually an expression of type :-> (closure),
-    # and its first arg is the args list (should be () ), followed by
-    # the array of statements in the closure's block
-    steps = reverse(blk.args[2].args)
-    pipes = map(steps) do p
-        if typeof(p) == Expr
-            :( it = chain!($(esc(p)), it) )
-        else
-            p
-        end
-    end
-    return quote
-        let collector = events()
-            it = collector
-            $(pipes...)
-            collector
-        end
-    end
-end
-
-# / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / 
-#
-# Operators for creating new types of `Observable`s
-#
-# / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / 
-
-"""
-Create a `Reactor` useful as the initial `Observable` in a reactive pipeline.
-"""
-function slot(initial)
-    Reactor(pass, [], initial)
-end
-
-function setSlotValue(s, value)
-    s.state = value
-    onEvent(s.reactor, getSlotValue(s))
-end
-
-function getSlotValue(s)
-    s.state
 end
 
 """
@@ -268,12 +200,71 @@ end
 function Base.IteratorSize(collector::EventCollector)
     Base.SizeUnknown()
 end
+"""
+Given a do-block where each statement is an `Observable`, 
+`subscribe!` each in sequence to the one proceeding. Return an
+object that one can use to iterate over the events from
+the last `Observable` in the block.
+
+Example:
+
+  ```
+  results = @rx() do
+    count
+    filter(:even)
+  end
+  for evt in results
+    # do something with evt
+  end
+  ```
+"""
+macro rx(blk)
+    # the blk is actually an expression of type :-> (closure),
+    # and its first arg is the args list (should be () ), followed by
+    # the array of statements in the closure's block
+    steps = reverse(blk.args[2].args)
+    pipes = map(steps) do p
+        :( it = chain!($(esc(p)), it) )
+    end
+    return quote
+        let collector = events()
+            it = collector
+            $(pipes...)
+            collector
+        end
+    end
+end
+
+# / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / 
+#
+# Various types of `Observable`s
+#
+# / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / 
+
+"""
+Treat any value as an `Observable`
+"""
+function subscribe!(value, observer)
+    begin 
+        try
+            evt = ValueEvent(value)
+            onEvent(observer, evt)
+            onComplete(observer)
+        catch e
+            println(stderr, "Caught error $e)")
+            for (exc, bt) in Base.catch_stack()
+                showerror(stdout, exc, bt)
+                println()
+            end            
+        end
+    end
+end
 
 """
 Treat an `Array` as an `Observable`
 """
 function subscribe!(observable::Array, observer)
-    @async begin 
+    begin 
         try
             for item in observable
                 evt = ValueEvent(item)
--- test/runtests.jl
+++ test/runtests.jl
@@ -12,15 +12,20 @@ end
 
 function plus3(observers, x)
     evt = ValueEvent(x + 3)
-    for observer in observers
-        onEvent(observer, evt)
-    end
+    notify!(observers, evt)
 end
 
 @test [4,5,6] == begin
     evts = @rx() do
         [1,2,3]
         Reactor(plus3)
+    end
+    [evt for evt in evts]  
+end
+
+@test [1] == begin
+    evts = @rx() do
+        1
     end
     [evt for evt in evts]  
 end
