6b62c043114e28f3118b5fb97398d4218d7f028a
Phil Hargett <phil@haphazardhouse.net>
2020-07-21 17:36:19 -0700

    adding doc serving

--- scripts/docs.jl
+++ scripts/docs.jl
@@ -1,19 +1,21 @@
 #! /usr/bin/env julia --color=yes
 
 using Base.Filesystem
+using Pkg
 
 scriptsDir = abspath(dirname(@__FILE__))
 baseDir = abspath(joinpath(scriptsDir, "../"))
 srcDir = abspath(joinpath(baseDir, "src/"))
 docsDir = abspath(joinpath(scriptsDir, "../docs/"))
 docSrcDir = abspath(joinpath(docsDir, "src/"))
+buildDir = abspath(joinpath(scriptsDir, "../docs/build/"))
 
-if !in(baseDir, LOAD_PATH)
-  push!(LOAD_PATH, baseDir)
-end
+Pkg.activate(docsDir)
 
 using FileWatching
 using Logging
+using HTTP
+using Sockets
 
 function watchDir(changes,dir)
   @async begin
@@ -25,18 +27,66 @@ function watchDir(changes,dir)
   end
 end
 
+function contentType(filePath)
+  if filePath[end-4:end] == ".html"
+    return "text/html"
+  elseif filePath[end-2:end] == ".js"
+    return "text/javascript"
+  elseif filePath[end-3:end] == ".css"
+    return "text/css"
+  elseif (filePath[end-3:end] == ".jpg" || filePath[end-4:end] == ".jpeg")
+    return "image/jpeg"
+  elseif filePath[end-3:end] == ".png"
+    return "image/png"
+  elseif filePath[end-4:end] == ".json"
+    return "application/json"
+  elseif filePath[end-3:end] == ".ico"
+    return "image/vnd.microsoft.icon"
+  else
+    return "application/octet-stream"
+  end
+end
+
+function serveDir(dir)
+  @async begin
+    HTTP.serve(Sockets.localhost,8080) do req::HTTP.Request
+        path = req.target
+        @info "Serving $path"
+        if path[begin] == '/'
+          path = path[begin+1:end]
+        end
+        filePath = abspath(joinpath(buildDir,path))
+        if filePath[end] == '/'
+          filePath *= "index.html"
+        end
+        if ispath(filePath)
+          response = HTTP.Response(200, read(filePath))
+          HTTP.setheader(response,"Content-Type" => contentType(filePath))
+          return response
+        else
+          return HTTP.Response(404,"Not found")
+        end
+    end  
+  end
+end
+
 @sync begin
   changes = Channel()
   watchDir(changes, docSrcDir)
   watchDir(changes, srcDir)
+  serveDir(buildDir)
   @async begin
     @info ">>>  Triggering initial generation of documentation"
     put!(changes, :first)
   end
   for change in changes
     cd(docsDir) do
-      run(`julia --color=yes make.jl`)
+      try
+        run(`julia --color=yes make.jl`)
+        @info ">>>  Generation complete"
+      catch ex
+        @error "!!!  Error generating docs: $ex"
+      end
     end
-    @info ">>>  Generation complete"
   end
 end
